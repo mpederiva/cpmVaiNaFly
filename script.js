5b92cb0a47f5d9120f9a7e7d7aafeaca46188658
Marta Pederiva <80361071+mpederiva@users.noreply.github.com>
2022-01-19 18:50:44 -0300

    upload everything

---
+++ script.js
@@ -0,0 +1,27 @@
+function displayTempo(){
+    var arrow = document.getElementById('animation');
+    arrow.classList.toggle('rotate');
+    var display = document.getElementById('tempo');
+    display.classList.toggle('active');
+}
+
+function displayGarantia(){
+    var arrow = document.getElementById('animati');
+    arrow.classList.toggle('rotate');
+    var display = document.getElementById('garantia');
+    display.classList.toggle('active');
+}
+function displayAcesso(){
+    var arrow = document.getElementById('anima');
+    arrow.classList.toggle('rotate');
+    var display = document.getElementById('acesso');
+    display.classList.toggle('active');
+    console.log (display);
+}
+
+function displayLocal(){
+    var arrow = document.getElementById('ani');
+    arrow.classList.toggle('rotate');
+    var display = document.getElementById('local');
+    display.classList.toggle('active');
+}
